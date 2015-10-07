7a1ddeb7f24dc4e8a7d91eec56a8bb2fb58ddaa5
Duncan McGreggor <oubiwann@gmail.com>
2015-10-07 17:14:11 -0500

    Added user agent and other consts.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject lcmap-client "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/USGS-EROS/lcmap-client-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]
+                 [leiningen-core "2.5.3"]])
--- src/lcmap_client/core.clj
+++ src/lcmap_client/core.clj
@@ -1,4 +1,17 @@
-(ns lcmap-client.core)
+(ns lcmap-client.core
+  (:require [leiningen.core.project :as lein]))
+
+(def version (System/getProperty "lcmap-client.version"))
+(def project-url (:url (lein/read)))
+(def user-agent (str "LCMAP REST Client/"
+                     version
+                     " (Clojure "
+                     (clojure-version)
+                     "; Java "
+                     (System/getProperty "java.version")
+                     ") (+"
+                     project-url
+                     ")"))
 
 (defn foo
   "I don't do a whole lot."
