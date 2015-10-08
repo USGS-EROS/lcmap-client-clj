b9f342b2702641fe44b29d3c43c2c0fbaa5d0bc1
Duncan McGreggor <oubiwann@gmail.com>
2015-10-08 12:26:41 -0500

    Added functions for taking the points/extents in the other direction.

--- src/lcmap_client/util.clj
+++ src/lcmap_client/util.clj
@@ -1,12 +1,19 @@
 (ns lcmap-client.util
-  (:require [clojure.string :as string])
+  (:require [clojure.string :as string]))
 
-(defn parse-point [param-str]
+(defn str->point [param-str]
   (transduce
     (map #(Integer/parseInt %))
     conj
     (string/split param-str #",")))
 
-(defn parse-extent [param-str]
-  (map #(parse-point %)
+(defn str->extent [param-str]
+  (map #(str->point %)
        (string/split param-str #";")))
+
+(defn point->str [[x y :as point]]
+  (string/join "," point))
+
+(defn extent->str [[[x1 y1 :as p1] [x2 y2 :as p2]]]
+  (string/join ";" (map #(point->str %)
+                        (list p1 p2))))
