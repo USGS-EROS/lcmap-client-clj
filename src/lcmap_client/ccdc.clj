95ff3dc614a0b47ef7e09cb473e71e4a1e048ffa
Duncan McGreggor <oubiwann@gmail.com>
2015-10-22 13:53:08 -0500

    Added placeholders for ccdc.

--- src/lcmap_client/ccdc.clj
+++ src/lcmap_client/ccdc.clj
@@ -1,6 +1,7 @@
 (ns lcmap-client.ccdc
   (:require [clojure.tools.logging :as log]
-            [lcmap-client.http :as lcmap]))
+            [lcmap-client.http :as http]
+            [lcmap-client.lcmap :as lcmap]))
 
 ;; Note that the client endpoint is defined  using the "/api" prefix, so the
 ;; following context is appended to that.
--- src/lcmap_client/ccdc/model.clj
+++ src/lcmap_client/ccdc/model.clj
@@ -1,6 +1,8 @@
 (ns lcmap-client.ccdc.model
   (:require [clojure.tools.logging :as log]
-            [lcmap-client.ccdc :as ccdc]))
+            [lcmap-client.ccdc :as ccdc]
+            [lcmap-client.http :as http]
+            [lcmap-client.lcmap :as lcmap]))
 
 ;; Note that the client endpoint is defined  using the "/api" prefix, so the
 ;; following context is appended to that.
