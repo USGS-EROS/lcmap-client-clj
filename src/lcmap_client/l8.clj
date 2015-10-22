082a01ac9f15d7732c6b6bafabe8b487019c64c1
Duncan McGreggor <oubiwann@gmail.com>
2015-10-22 13:52:45 -0500

    Updated l8 namespace.

--- src/lcmap_client/l8.clj
+++ src/lcmap_client/l8.clj
@@ -1,8 +1,8 @@
 (ns lcmap-client.l8
   (:require [clojure.tools.logging :as log]
-            [lcmap-client.http :as lcmap]))
+            [lcmap-client.lcmap :as lcmap]))
 
-;; Note that the client endpoint is defined  using the "/api" prefix, so the
+;; Note that the client endpoint is defined using the "/api" prefix, so the
 ;; following context is appended to that.
 
 (def context (str lcmap/context "/L1/T/Landsat/8"))
