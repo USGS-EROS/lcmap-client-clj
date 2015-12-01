61418dc118d28abbf7e38d3f5c367536f3b20024
Duncan McGreggor <dmcgreggor@usgs.gov>
2015-12-01 17:42:04 -0600

    Removed data code from model.

--- src/lcmap_client/models/surface_reflectance.clj
+++ src/lcmap_client/models/surface_reflectance.clj
@@ -10,22 +10,3 @@
   (http/get (str context "/")
             :lcmap-opts (or args {})))
 
-(defn get-tiles [& {:keys [point extent time band] :as args}]
-  (let [[point extent] (util/update-point-extent point extent)
-        args (apply dissoc args [:point :extent :time :band])]
-    (http/get (str context "/tiles")
-              :lcmap-opts args
-              :request {:query-params {:point point
-                                       :extent extent
-                                       :time time
-                                       :band band}})))
-
-(defn get-rod [& {:keys [point time band] :as args}]
-  (let [args (apply dissoc args [:point :time :band])]
-    (http/get (str context "/rod")
-              :lcmap-opts args
-              :request {:query-params {:point (util/point->str point)
-                                       :time time
-                                       :band band}})))
-
-
