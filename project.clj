6aa6f0bb81f8861a86c5b0b3c979ee3d3d58a5d2
Duncan McGreggor <dmcgreggor@usgs.gov>
2015-12-04 15:19:41 -0600

    Added support for fp-style error handling.

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [clj-http "2.0.0"]
+                 [dire "0.5.3"]
                  [leiningen-core "2.5.3"]
                  [clojure-ini "0.0.2"]
                  [org.clojure/tools.namespace "0.2.11"]]
