63d5caace64aff0f16e2eefeb740db2ef3f71fdf
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-01-12 12:42:13 -0600

    Added docker sample model support.

---
+++ src/lcmap_client/models/sample_docker_process.clj
@@ -0,0 +1,20 @@
+(ns lcmap-client.models.sample-docker-process
+  (:require [clojure.tools.logging :as log]
+            [lcmap-client.http :as http]
+            [lcmap-client.models :as models]))
+
+;; Note that the client endpoint is defined  using the "/api" prefix, so the
+;; following context is appended to that.
+
+(def context (str models/context "/sample/docker-process"))
+
+(defn get-resources [client & {keys [] :as args}]
+  (http/get (str context "/")
+            :client client
+            :lcmap-opts (or args {})))
+
+(defn run [client & {:keys [year docker-tag] :as args}]
+  (http/post context
+             :client client
+             :clj-http-opts {:form-params {:year year :delay delay}}
+             :lcmap-opts args))
