832adcb03c2454f71a212f5a78fd28a8ae078915
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-01-29 12:52:03 -0600

    Added CCDC docker process support.

---
+++ src/lcmap_client/models/ccdc_docker_process.clj
@@ -0,0 +1,20 @@
+(ns lcmap-client.models.ccdc-docker-process
+  (:require [clojure.tools.logging :as log]
+            [lcmap-client.http :as http]
+            [lcmap-client.models :as models]))
+
+;; Note that the client endpoint is defined  using the "/api" prefix, so the
+;; following context is appended to that.
+
+(def context (str models/context "/ccdc/docker-process"))
+
+(defn get-resources [client & {keys [] :as args}]
+  (http/get (str context "/")
+            :client client
+            :lcmap-opts (or args {})))
+
+(defn run [client & {:keys [arg1 arg2] :as args}]
+  (http/post context
+             :client client
+             :clj-http-opts {:form-params {:arg-1 arg1 :arg-2 arg2}}
+             :lcmap-opts args))
