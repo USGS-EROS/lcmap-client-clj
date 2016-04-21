e6468b71f2d750d936de1382def6d605b31c4dd1
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-04-21 13:50:33 -0500

    Added more status codes.

--- src/lcmap/client/status_codes.clj
+++ src/lcmap/client/status_codes.clj
@@ -11,8 +11,11 @@
 (def permanant-link 308)
 
 ;; 4xx
+(def bad-request 400)
+(def client-error 400)
 (def unauthorized 401)
 (def forbidden 403)
+(def not-found 404)
 (def no-resource 404)
 (def auth-timeout 419)
 (def token-invalid 498)
@@ -32,9 +35,12 @@
 (defn permanant-link? [status] (= status permanant-link))
 
 ;; 4xx
+(defn bad-request?[status] (= status bad-request))
+(defn client-error?[status] (= status client-error))
 (defn unauthorized?[status] (= status unauthorized))
 (defn forbidden?[status] (= status forbidden))
 (defn forbidden?[status] (= status forbidden))
+(defn not-found? [status] (= status not-found))
 (defn no-resource? [status] (= status no-resource))
 (defn auth-timeout? [status] (= status auth-timeout))
 (defn token-invalid? [status] (= status token-invalid))
