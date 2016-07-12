1a8e318dafdbba3c2a0b4af932d8e2b6735ebb6b
Duncan McGreggor <duncan.mcgreggor.ctr@usgs.gov>
2016-07-12 11:47:50 -0500

    Updated deps.

--- project.clj
+++ project.clj
@@ -5,14 +5,15 @@
             :url "http://ti.arc.nasa.gov/opensource/nosa/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/core.memoize "0.5.8"]
+                 [org.clojure/core.memoize "0.5.9"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.3"]
-                 [com.stuartsierra/component "0.3.0"]
-                 [clj-http "2.0.0"]
-                 [dire "0.5.3"]
-                 [leiningen-core "2.5.3"]
+                 [clojusc/twig "0.2.1"]
+                 ;;[ch.qos.logback/logback-classic "1.1.3"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [clj-http "3.1.0"]
+                 [dire "0.5.4"]
+                 [leiningen-core "2.6.1"]
                  [clojure-ini "0.0.2"]
                  ;; LCMAP Components
                  [gov.usgs.eros/lcmap-config "0.5.0-SNAPSHOT"]
@@ -24,7 +25,7 @@
         :log-level :trace
         :logging-namespaces [lcmap.client]}
       :dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
-                     [pandect "0.5.4"]
+                     [pandect "0.6.0"]
                      [slamhound "1.5.5"]]
       :plugins [[lein-kibit "0.1.2"]]
       :aliases {"slamhound" ["run" "-m" "slam.hound"]}
