fdd4a9ae9c2d7d20a38054b53b0b8101b53d37f1
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-08-31 19:38:17 -0500

    Updated travis to work with managed-deps and lein-parent.

--- test/support/setup_deps.sh
+++ test/support/setup_deps.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+mkdir checkouts
+cd checkouts && \
+    git clone https://github.com/USGS-EROS/lcmap-config.git && \
+    git clone https://github.com/USGS-EROS/lcmap-logger.git && \
+    cd ../
+
 mkdir ~/.usgs/
 cp test/support/sample_config.ini ~/.usgs/lcmap.ini
+
